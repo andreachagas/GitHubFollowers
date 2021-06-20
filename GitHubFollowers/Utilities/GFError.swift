4b71cbc6537c15bad863ac860781478c10d56e31
Andrea <andrea.lima.chagas@gmail.com>
2021-06-20 22:00:59 +0800

    Made some minor changes, tidying code up

--- GitHubFollowers/Utilities/GFError.swift
+++ GitHubFollowers/Utilities/GFError.swift
@@ -1,5 +1,5 @@
 //
-//  ErrorMessage.swift
+//  GFError.swift
 //  GitHubFollowers
 //
 //  Created by Andrea Chagas on 20/6/21.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum ErrorMessage: String {
+enum GFError: String, Error {
     case invalidUsername = "This username created an invalid request, please try again."
     case unableToComplete = "Unable to complete your request. Please check your internet connection."
     case invalidResponse = "Invalid response from the server. Please try again."
