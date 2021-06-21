99942f30184dfce3d488af91e01226ef3d293bab
Andrea <andrea.lima.chagas@gmail.com>
2021-06-21 10:39:36 +0800

    Updating FollowersListVC and fixing bug

--- GitHubFollowers/Views/FollowerListVC.swift
+++ GitHubFollowers/Views/FollowerListVC.swift
@@ -9,15 +9,23 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
+    var followers: [Follower] = [Follower]()
+    
     var username: String = ""
     var collectionView: UICollectionView!
-
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureViewController()
         configureCollectionView()
         getFollowers()
+        configureDataSource()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -32,22 +40,53 @@ class FollowerListVC: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout()) // fill the entire view up to bounds
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout()) // fill the entire view up to bounds
         view.addSubview(collectionView)
-        collectionView.backgroundColor = .systemRed 
+        collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width // total width of the screen
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2) // left and right padding, and middle spaces
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40) // label is 20 height, so gives a symetrical look
+        
+        return flowLayout
     }
     
     func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            
             switch result {
             case .success(let followers):
-                print(followers)
+                self.followers = followers // adding all followers
+                self.updateData() // avoiding race conditions
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
         }
     }
-
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower) // sets username to the label as the login name
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapshot, animatingDifferences: true) // to avoid warnings from Xcode, although WWDC said it was fine to call from back thread
+        }
+    }
 }
